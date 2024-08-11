890e952b259d0d151794ffb31d3f00c8a0fb869f
angierkd <everydayangie.k@gmail.com>
2024-08-12 07:35:48 +0900

    infra: docker image 생성 자동화

--- src/main/java/com/shopping/book/common/aspect/ErrorLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shopping.book.common.aspect;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ErrorLogRepository extends ElasticsearchRepository<ErrorLog, String> {
-}
--- src/main/java/com/shopping/book/common/aspect/LoggingAspect.java
+++ src/main/java/com/shopping/book/common/aspect/LoggingAspect.java
@@ -3,7 +3,6 @@ package com.shopping.book.common.aspect;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -12,9 +11,6 @@ import java.util.Arrays;
 @Aspect
 @Component
 public class LoggingAspect {
-
-    @Autowired
-    private ErrorLogRepository errorLogRepository;
 
     // 공통 포인트컷 정의
     @Pointcut("execution(* com.shopping.book..controller.*.*(..)) || " +
@@ -40,19 +36,6 @@ public class LoggingAspect {
     @AfterThrowing(pointcut = "applicationPackagePointcut()", throwing = "exception")
     public void logException(JoinPoint joinPoint, Throwable exception) {
         String methodName = joinPoint.getSignature().getName();
-        String className = joinPoint.getSignature().getDeclaringTypeName();
-        String arguments = Arrays.toString(joinPoint.getArgs());
-        String stackTrace = Arrays.toString(exception.getStackTrace());
-
-        log.error("Exception in method: {} in class: {} with arguments: {} and message: {}",
-                methodName,
-                className,
-                arguments,
-                exception.getMessage(),
-                exception);
-
-        // 예외 정보를 Elasticsearch에 저장
-        ErrorLog errorLog = new ErrorLog(exception.getMessage(), stackTrace, className, methodName, arguments);
-        errorLogRepository.save(errorLog);
+        log.error("Exception in method: {} with message: {}", methodName, exception.getMessage(), exception);
     }
 }
